df51e8ccbbdfeec065331c58a538613c089ec779
Felix <felix@felixsoum.com>
2020-05-12 04:23:03 -0400

    Add monster duel and arena

---
+++ MonsterArena/MonsterArena/ArenaMaster.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MonsterArena
+{
+    class ArenaMaster
+    {
+        private static ArenaMaster instance;
+        private static List<Monster> monsters = new List<Monster>();
+
+        private ArenaMaster() { }
+
+        public static ArenaMaster GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new ArenaMaster();
+            }
+
+            return instance;
+        }
+
+        public void SetMonsterList(List<Monster> monsters)
+        {
+            ArenaMaster.monsters = monsters;
+        }
+
+        public List<MonsterData> GetMonsterList()
+        {
+            return new List<MonsterData>(monsters.Select(m => m.GetData()));
+        }
+    }
+}
---
+++ MonsterArena/MonsterArena/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MonsterArena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Monster> monsters = new List<Monster>()
+            {
+                new Goblin("Alice"),
+                new Orc("Bob"),
+                new Goblin("Charlie"),
+                new Orc("David")
+            };
+
+            foreach (var monster in monsters)
+            {
+                monster.Spawn();
+            }
+
+            ArenaMaster.GetInstance().SetMonsterList(monsters);
+
+            while (!IsBattleOver(monsters))
+            {
+                monsters.Sort();
+                foreach (var activeMonster in monsters)
+                {
+                    if (activeMonster.IsDead())
+                        continue;
+
+                    int attackIndex = activeMonster.GetAttackIndex();
+                    if (attackIndex < 0 || attackIndex >= monsters.Count)
+                        continue;
+
+                    activeMonster.Attack(monsters[attackIndex]);
+                }
+            }
+
+            Monster winner = monsters.Find(m => !m.IsDead());
+            if (winner != null)
+            {
+                Console.WriteLine($"The winner is {winner}!");
+            }
+            else
+            {
+                Console.WriteLine("All combatants have perished...");
+            }
+        }
+
+        static bool IsBattleOver(List<Monster> monsters)
+        {
+            return monsters.FindAll(m => !m.IsDead()).Count <= 1;
+        }
+    }
+}
