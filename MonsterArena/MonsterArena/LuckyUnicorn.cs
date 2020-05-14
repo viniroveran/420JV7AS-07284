4a19aa866615e1406ac1599a9c14322457bfa65a
Vinicius Roveran Ribeiro <viniroveran@gmail.com>
2020-05-14 13:35:29 -0400

    Add LuckyUnicorn

---
+++ MonsterArena/MonsterArena/LuckyUnicorn.cs
@@ -0,0 +1,23 @@
+﻿namespace MonsterArena
+{
+    class LuckyUnicorn : Monster
+    {
+        public LuckyUnicorn(string name) : base(name)
+        {
+            AddBonusStrength(25);
+            AddBonusLuck(75);
+        }
+        public override int GetAttackIndex()
+        {
+            int index = ArenaMaster.GetInstance().GetMonsterList().FindIndex(m => m.type == "Orc");
+            if (index >= 0)
+            {
+                return index;
+            }
+            else
+            {
+                return base.GetAttackIndex();
+            }
+        }
+    }
+}
