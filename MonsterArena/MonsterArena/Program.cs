a12f937d046c59a97f7cfb9e6718b7d161c73905
songxiangfeng <fengxs123@gmail.com>
2020-05-14 13:48:10 -0400

    Add Dragon

--- MonsterArena/MonsterArena/Program.cs
+++ MonsterArena/MonsterArena/Program.cs
@@ -12,7 +12,8 @@ namespace MonsterArena
                 new Goblin("Alice"),
                 new Orc("Bob"),
                 new Goblin("Charlie"),
-                new Orc("David")
+                new Orc("David"),
+                new Dragon("Xiangfeng")
             };
 
             foreach (var monster in monsters)
